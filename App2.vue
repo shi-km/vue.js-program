6c08702043ebb23c520104e7a10f005449c5be66
shi-km <kamaujoj98@gmail.com>
2025-02-20 09:24:22 +0300

    initial commit

---
+++ App2.vue
@@ -0,0 +1,48 @@
+<!-- where the javascript falls under -->
+<script>
+   export default{
+    data(){
+       return {
+        name:    'George Kamau',
+        status:  "pending",
+        tasks:   ['task one','task two','task three','task four','task five'],
+        link : 'https://timazi.org'
+       }
+    },
+     methods:{
+
+      toogleFunction(){
+        if(this.status === 'active'){
+          this.status = 'pending';
+        }
+        else if(this.status === 'pending'){
+          this.status = 'active'
+        }
+        else {
+          this.status = 'active';
+        }
+      }
+     }
+  };
+  
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is Active</p>
+  <p v-else-if="status ==='pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{task}}</li>
+    <a v-bind:href="link">Click for Google</a> <br>
+
+    <button @click="toogleFunction">Change status</button>
+  </ul>
+</template>
+
+<!-- Where the styling falls under -->
+<style scoped>
+
+</style>
---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<!-- where the javascript falls under -->
+<script setup>
+import {ref} from 'vue';
+  
+    const name =ref('jane Doe');
+    const status = ref('active');
+    const tasks  =ref(['task one','task two', 'task three',  'task four']);
+    const toogleFunction = () => {
+      if(status.value === 'active'){
+          status.value = 'pending';
+        }
+        else if(status.value === 'pending'){
+          status.value = 'active'
+        }
+        else {
+          status.value = 'active';
+        }
+      };
+ 
+  
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === 'active'">User is Active</p>
+  <p v-else-if="status ==='pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3>Tasks:</h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">{{task}}</li>
+    <a v-bind:href="link">Click for Google</a> <br>
+
+    <button @click="toogleFunction">Change status</button>
+  </ul>
+</template>
+
+<!-- Where the styling falls under -->
+<style scoped>
+
+</style>
