4e843b1906ff95cc54136b28b2c81d8f8a17ec60
shi-km <kamaujoj98@gmail.com>
2025-02-27 15:46:50 +0300

    modified my html file

--- src/App.vue
+++ src/App.vue
@@ -2,40 +2,30 @@
 <script setup>
 import {ref} from 'vue';
   
-    const name =ref('jane Doe');
-    const status = ref('active');
-    const tasks  =ref(['task one','task two', 'task three',  'task four']);
-    const toogleFunction = () => {
-      if(status.value === 'active'){
-          status.value = 'pending';
-        }
-        else if(status.value === 'pending'){
-          status.value = 'active'
-        }
-        else {
-          status.value = 'active';
-        }
-      };
- 
-  
+    const newTask = ref('');
+    const tasks  = ref(['Task one','Task two', 'Task three',  'Task four']);
+    const addTasks = () => {
+
+      if(newTask.value.trim() !== ''){
+         tasks.value.push(newTask.value);
+         newTask.value = '';
+      }
+    }
 </script>
 
 <template>
-  <h1>{{ name }}</h1>
-  <p v-if="status === 'active'">User is Active</p>
-  <p v-else-if="status ==='pending'">User is pending</p>
-  <p v-else>User is inactive</p>
-
   <h3>Tasks:</h3>
+    <form @submit.prevent="addTasks">
+      <label for="">Add Task: </label>
+      <input type="text" id="taskname" name="newtask" v-model="newTask ">
+      <button>Add Task</button>
+    </form>
   <ul>
-    <li v-for="task in tasks" :key="task">{{task}}</li>
-    <a v-bind:href="link">Click for Google</a> <br>
-
-    <button @click="toogleFunction">Change status</button>
+    <li v-for="task in tasks" :key="tasks">{{ task }}</li>
   </ul>
 </template>
 
-<!-- Where the styling falls under -->
+
 <style scoped>
 
 </style>
